7e7b2d41a372620ff92b220535718db44e48b3c2
Mfundo Sindane <mfundosindane0@gmail.com>
2022-12-18 00:20:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //method for contact detail animation:
 fotter();
 
-function fotter() {
-    
+function fotter() {    
     const inView = (object)=>{
         object.forEach(element => {
         element.target.classList.toggle('is-inViewport',element.isIntersecting);
@@ -19,22 +18,21 @@ element.forEach(el=>{
 
 //three.js code
 //essential three.js components:
+    
 const canvas=document.getElementById('canvas');
-const width =canvas.offsetWidth;
-const height =canvas.offsetHeight;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  70,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+70,
+window.innerWidth / window.innerHeight,
+0.1,
+1000
 );
 
 camera.position.set(10,10,10);
 camera.lookAt(0,0,0);
 
 
-const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true});
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setClearColor(0x000000);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -43,23 +41,25 @@ const material = new THREE.MeshNormalMaterial({wireframe:true});
 const shape = new THREE.Mesh(geometry,material);
 scene.add(shape);
 
-//animation method:
 update();
 
+
+//animation method:
 function update() {
     renderer.render(scene,camera);
+    canvas.style.width = 100+"%";
+    canvas.style.height = 100+"%";
     shape.rotateY(0.003);
     requestAnimationFrame(update);
-        
+    
 }
-
 
 //morph object at set time:
 //setTimeout(()=>{ morph(shape,2,2);},8000);
 
 
 //morph object method:
-function morph(object,width,height){
+/*function morph(object,width,height){
 let condition = false;
 let newGeometry = new THREE.SphereGeometry(8,width,height);
 let newMaterial = new THREE.MeshNormalMaterial({wireframe:condition});
@@ -74,3 +74,4 @@ object.traverse((obj)=>{
 });
 
 }
+*/
